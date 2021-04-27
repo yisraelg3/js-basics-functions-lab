fa43b2107b2a6b91628f085b2107d118243d79ed
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-27 09:43:40 -0400

    commit tree c47eb9f9df9e8036c8b36c7b7dd89b0def215f52

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (destination) {
+    const Hq = 42
+    const distanceInBlocks = Math.abs(Hq - parseInt(destination));
+    return distanceInBlocks;
+}
+
+function distanceFromHqInFeet (blocks) {
+    const amountOfFeet = distanceFromHqInBlocks(blocks) * 264;
+    return amountOfFeet;
+}
+
+function distanceTravelledInFeet (block1, block2) {
+    const distanceInBlocks = Math.abs(parseInt(block1) - parseInt(block2));
+    const distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+}
+
+function calculatesFarePrice (block1, block2) {
+    const distanceInFeet = distanceTravelledInFeet(block1, block2);
+    if (distanceInFeet <= 400) {
+        return 0;
+    }  else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return ((distanceInFeet - 400) * 2 / 100);
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500 ) {
+        return 25;
+    } else if ( distanceInFeet > 2500) {
+        return 'cannot travel that far';
+    }
+}
